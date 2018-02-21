d26d1bafe8a1aa2404403da0c30eaf5288260287
castelg <gianluca.castellani@kaust.edu.sa>
2018-02-21 10:43:51 +0300

    using Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,10 +1,27 @@
+import feedparser
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def get_news():
-      return "no news is good news"
+NEWS_FEEDS = {#'ARABNEWS' : 'http://www.arabnews.com/cat/1/rss.xml',
+              'BBC' : 'http://feeds.bbci.co.uk/news/rss.xml',
+              'CNN': 'http://rss.cnn.com/rss/edition.rss',
+              'FOX': 'http://feeds.foxnews.com/foxnews/latest',
+              #'REPUBBLICA': 'http://www.repubblica.it/rss/homepage/rss2.0.xml'
+              }
 
-  if __name__ == '__main__':
-        app.run(port=5000, debug=True)
+
+@app.route('/')
+#@app.route('/<publication>')
+def get_news(publication='BBC'):
+    articles = []
+    for pub in NEWS_FEEDS:
+        feed = feedparser.parse(NEWS_FEEDS[pub])
+        first_article = feed['entries'][0]
+        articles.append(first_article)
+    return render_template('home.html', articles=articles)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
